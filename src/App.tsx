4bada5de4a7ef4c10d8a06f3893517efb0faeb5d
jyh <yonghedawang@qq.com>
2022-11-14 17:29:15 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,48 @@ function App() {
 
   },[editor]);
 
+  const addStaticPath = React.useCallback(()=>{
+    //需要在nginx里配置一下跨域
+    if(editor){
+      editor.objects.add({
+        type:'StaticPath',
+        fill:'#CBCBCB',//https://tstatic.redocn.com/react/images/001-hug.svg  https://ik.imagekit.io/scenify/005-date.svg
+        path:[[ 60, 0]]
+      });
+    }
+
+  },[editor]);
+
+  //StaticGroup，DynamicGroup 不知道怎么操作，在 react-design-editor  搜不到相关代码
+
+/*   {
+    id: "E2mcHFkwGA-MTJcfl3Abs",
+    name: "StaticPath",
+    angle: 0,
+    stroke: null,
+    strokeWidth: 0,
+    left: 328,
+    top: 574.22,
+    width: 60,
+    height: 60,
+    opacity: 1,
+    originX: "left",
+    originY: "top",
+    scaleX: 9.19,
+    scaleY: 2,
+    type: "StaticPath",
+    flipX: false,
+    flipY: false,
+    skewX: 0,
+    skewY: 0,
+    visible: true,
+    shadow: null,
+    path: [["M", 60, 0], ["L", 0, 0], ["L", 0, 60], ["L", 60, 60], ["L", 60, 0], ["Z"]],
+    fill: "#ff4040",
+    metadata: {},
+  } */
+
+
   return (
     <div
       style={{
@@ -61,6 +103,7 @@ function App() {
         <button onClick={addText}>ADD TEXT</button>
         <button onClick={addImage}>ADD IMAGE</button>
         <button onClick={addStaticVector}>ADD StaticVector</button>
+        <button onClick={addStaticPath}>ADD StaticPath</button>
       </div>
       <div style={{ flex: 1, display:"flex" }}>
         <Canvas />
