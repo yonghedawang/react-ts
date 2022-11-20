934bd440d030fb872707601e17905f85bcde2f64
jyh <343970500@qq.com>
2022-11-20 22:02:48 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Canvas, useEditor, useActiveObject, useFrame } from "@layerhub-io/react";
-import { IFrame,IScene } from "@layerhub-io/types"
+import { IFrame, IScene } from "@layerhub-io/types"
 import { nanoid } from 'nanoid'
 
 function App() {
@@ -24,10 +24,10 @@ function App() {
   const [frameHeight, setFrameHeight] = React.useState(1000)
   const [backgroundColor, setBackgroundColor] = React.useState('#ffffff')
   const [scenes, setScenes] = React.useState<IScene[]>([]);
-  const [currentScene,setCurrentScene] = React.useState<IScene>();
-  const [currentDesign,setCurrentDesign] = React.useState<IDesign>();
-  const [currentPreview,setCurrentPreview] = React.useState<string>()
-  
+  const [currentScene, setCurrentScene] = React.useState<IScene>();
+  const [currentDesign, setCurrentDesign] = React.useState<IDesign>();
+  const [currentPreview, setCurrentPreview] = React.useState<string>()
+
 
   const loadGraphicTemplate = async (payload: IDesign) => {
     const scenes: IScene[] = []
@@ -41,10 +41,10 @@ function App() {
         layers: scn.layers,
         metadata: {},
       }
-      
-      
+
+
       const preview = (await editor.renderer.render(scene)) as string
-     // console.log(preview);
+      // console.log(preview);
       scenes.push({ ...scene, preview })
       // console.log(scenes[0].layers);
       setScenes(scenes)
@@ -57,22 +57,26 @@ function App() {
 
   React.useEffect(() => {
     if (editor) {
+      console.log("currentScene changed");
       editor.scene.importFromJSON(currentScene);
+
+      
+
     }
-    console.log("currentScene changed");
-  },[currentScene]);
+
+  }, [currentScene]);
 
   React.useEffect(() => {
-    
+
     console.log("useEffect scenes changed");
-  },[scenes]);
+  }, [scenes]);
 
   React.useEffect(() => {
-    
-    console.log("useEffect Canvas changed");
-  },[Canvas]);
 
-  
+    console.log("useEffect Canvas changed");
+  }, [Canvas]);
+
+
 
 
   //第一次加载
@@ -89,11 +93,11 @@ function App() {
       // const design: IScene = { "id": "98PZEYOW0oR-TbmSEQ-MI", "name": "test import", "frame": { "width": 1200, "height": 1200 }, "layers": [{ "id": "background", "name": "Initial Frame", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 0, "top": 0, "width": 1200, "height": 1200, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "Background", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": { "color": "#fcfcfc", "blur": 4, "offsetX": 0, "offsetY": 0, "affectStroke": false, "nonScaling": false }, "fill": "#ffffff", "metadata": {} }, { "id": "9VWsxoB_O8LSAkCIRjNFY", "name": "StaticPath", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 103.25999999999999, "top": 54.72999999999999, "width": 498.84, "height": 436.27, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 0.48, "scaleY": 0.48, "type": "StaticPath", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": null, "preview": "https://ik.imagekit.io/scenify/1635014101144_519480.png", "path": [["M", 497.434, 243.764], ["L", 377.754, 36.403999999999996], ["C", 376.05600000000004, 33.37, 372.90700000000004, 31.432999999999996, 369.434, 31.283999999999995], ["L", 130.71400000000003, 31.283999999999995], ["C", 127.02300000000002, 31.250999999999994, 123.59900000000003, 33.206999999999994, 121.75400000000002, 36.403999999999996], ["L", 1.434000000000026, 244.404], ["C", -0.477999999999974, 247.626, -0.477999999999974, 251.635, 1.434000000000026, 254.857], ["L", 121.75400000000002, 463.07], ["C", 123.65600000000002, 465.803, 126.74500000000002, 467.467, 130.074, 467.55], ["L", 369.434, 467.55], ["C", 372.77200000000005, 467.502, 375.877, 465.83, 377.754, 463.07], ["L", 497.434, 254.21699999999998], ["C", 499.302, 250.983, 499.302, 246.999, 497.434, 243.764], ["z"]], "fill": "#CBCBCB", "metadata": {} }, { "id": "WZLRN4LYlahg798kLogBm", "name": "StaticImage", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 401.28999999999996, "top": 275, "width": 650, "height": 650, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "StaticImage", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": null, "src": "https://images.pexels.com/photos/670741/pexels-photo-670741.jpeg?auto=compress&cs=tinysrgb&h=650&w=940", "cropX": 0, "cropY": 0, "metadata": {} }, { "id": "A6rI0zdsoHWc0Jaht2vDG", "name": "StaticText", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 415.79999999999995, "top": 75.13, "width": 577.32, "height": 103.96, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "StaticText", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": null, "charSpacing": 0, "fill": "#333333", "fontFamily": "OpenSans-Regular", "fontSize": 92, "lineHeight": 1.16, "text": "simple text", "textAlign": "center", "fontURL": "https://fonts.gstatic.com/s/opensans/v27/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0C4nY1M2xLER.ttf", "metadata": {} }, { "id": "Kv__-PW6UJAh9S6lDcg_l", "name": "StaticVector", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 111.48000000000002, "top": 396.73, "width": 512, "height": 512, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 0.45, "scaleY": 0.45, "type": "StaticVector", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": null, "src": "https://ik.imagekit.io/scenify/007-hug.svg", "colorMap": { "#231f20": "#231f20", "#007dc4": "#007dc4", "rgb(0,0,0)": "rgb(0,0,0)", "#fff": "#fff", "#ffc10e": "#ffc10e" }, "metadata": {} }], "metadata": {}, "preview": "" }
       // editor.scene.importFromJSON(design);
       // console.log(design);
-      const template = {previews:[],"published":true, "id": "N1zgp3tfX77pFGRhVAJne", "type": "GRAPHIC", "name": "Untitled Design", "frame": { "width": 1200, "height": 1200 }, "scenes": [{ "id": "qKDtWwlWgZsUQ6tvRIaWX", "layers": [{ "id": "background", "name": "Initial Frame", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 0, "top": 0, "width": 1200, "height": 1200, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "Background", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": { "color": "#fcfcfc", "blur": 4, "offsetX": 0, "offsetY": 0, "affectStroke": false, "nonScaling": false }, "fill": "#ffffff", "metadata": {} }, { "id": "geF55nXZonGCHE7qz_CC1", "name": "StaticText", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 409.13, "top": 489.81, "width": 420, "height": 103.96, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "StaticText", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": null, "charSpacing": 0, "fill": "#333333", "fontFamily": "OpenSans-Regular", "fontSize": 92, "lineHeight": 1.16, "text": "scene1", "textAlign": "center", "fontURL": "https://fonts.gstatic.com/s/opensans/v27/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0C4nY1M2xLER.ttf", "metadata": {} }], "name": "Untitled design" }, { "id": "9SAW3VwN-Wf47aDgQb3Ml", "layers": [{ "id": "background", "name": "Initial Frame", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 0, "top": 0, "width": 1200, "height": 1200, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "Background", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": { "color": "#fcfcfc", "blur": 4, "offsetX": 0, "offsetY": 0, "affectStroke": false, "nonScaling": false }, "fill": "#ffffff", "metadata": {} }, { "id": "TuUsMXh-7BX2LpdZs3Cq2", "name": "StaticText", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 419.13, "top": 499.81, "width": 420, "height": 103.96, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "StaticText", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": null, "charSpacing": 0, "fill": "#333333", "fontFamily": "OpenSans-Regular", "fontSize": 92, "lineHeight": 1.16, "text": "scene2", "textAlign": "center", "fontURL": "https://fonts.gstatic.com/s/opensans/v27/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0C4nY1M2xLER.ttf" }], "name": "Untitled design" }, { "id": "BlZ2k3NWrDJIOlGleOvnA", "layers": [{ "id": "background", "name": "Initial Frame", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 0, "top": 0, "width": 1200, "height": 1200, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "Background", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": { "color": "#fcfcfc", "blur": 4, "offsetX": 0, "offsetY": 0, "affectStroke": false, "nonScaling": false }, "fill": "#ffffff", "metadata": {} }, { "id": "1y4hHIZFAA12vI13d8Xgj", "name": "StaticText", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 429.13, "top": 509.81, "width": 420, "height": 103.96, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "StaticText", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": null, "charSpacing": 0, "fill": "#333333", "fontFamily": "OpenSans-Regular", "fontSize": 92, "lineHeight": 1.16, "text": "scene3", "textAlign": "center", "fontURL": "https://fonts.gstatic.com/s/opensans/v27/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0C4nY1M2xLER.ttf" }], "name": "Untitled design" }], "metadata": {}, "preview": "" };
-      const loadedDesign =  loadGraphicTemplate(template);
+      const template = { previews: [], "published": true, "id": "N1zgp3tfX77pFGRhVAJne", "type": "GRAPHIC", "name": "Untitled Design", "frame": { "width": 1200, "height": 1200 }, "scenes": [{ "id": "qKDtWwlWgZsUQ6tvRIaWX", "layers": [{ "id": "background", "name": "Initial Frame", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 0, "top": 0, "width": 1200, "height": 1200, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "Background", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": { "color": "#fcfcfc", "blur": 4, "offsetX": 0, "offsetY": 0, "affectStroke": false, "nonScaling": false }, "fill": "#ffffff", "metadata": {} }, { "id": "geF55nXZonGCHE7qz_CC1", "name": "StaticText", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 409.13, "top": 489.81, "width": 420, "height": 103.96, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "StaticText", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": null, "charSpacing": 0, "fill": "#333333", "fontFamily": "OpenSans-Regular", "fontSize": 92, "lineHeight": 1.16, "text": "scene1", "textAlign": "center", "fontURL": "https://fonts.gstatic.com/s/opensans/v27/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0C4nY1M2xLER.ttf", "metadata": {} }], "name": "Untitled design" }, { "id": "9SAW3VwN-Wf47aDgQb3Ml", "layers": [{ "id": "background", "name": "Initial Frame", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 0, "top": 0, "width": 1200, "height": 1200, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "Background", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": { "color": "#fcfcfc", "blur": 4, "offsetX": 0, "offsetY": 0, "affectStroke": false, "nonScaling": false }, "fill": "#ffffff", "metadata": {} }, { "id": "TuUsMXh-7BX2LpdZs3Cq2", "name": "StaticText", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 419.13, "top": 499.81, "width": 420, "height": 103.96, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "StaticText", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": null, "charSpacing": 0, "fill": "#333333", "fontFamily": "OpenSans-Regular", "fontSize": 92, "lineHeight": 1.16, "text": "scene2", "textAlign": "center", "fontURL": "https://fonts.gstatic.com/s/opensans/v27/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0C4nY1M2xLER.ttf" }], "name": "Untitled design" }, { "id": "BlZ2k3NWrDJIOlGleOvnA", "layers": [{ "id": "background", "name": "Initial Frame", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 0, "top": 0, "width": 1200, "height": 1200, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "Background", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": { "color": "#fcfcfc", "blur": 4, "offsetX": 0, "offsetY": 0, "affectStroke": false, "nonScaling": false }, "fill": "#ffffff", "metadata": {} }, { "id": "1y4hHIZFAA12vI13d8Xgj", "name": "StaticText", "angle": 0, "stroke": null, "strokeWidth": 0, "left": 429.13, "top": 509.81, "width": 420, "height": 103.96, "opacity": 1, "originX": "left", "originY": "top", "scaleX": 1, "scaleY": 1, "type": "StaticText", "flipX": false, "flipY": false, "skewX": 0, "skewY": 0, "visible": true, "shadow": null, "charSpacing": 0, "fill": "#333333", "fontFamily": "OpenSans-Regular", "fontSize": 92, "lineHeight": 1.16, "text": "scene3", "textAlign": "center", "fontURL": "https://fonts.gstatic.com/s/opensans/v27/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0C4nY1M2xLER.ttf" }], "name": "Untitled design" }], "metadata": {}, "preview": "" };
+      const loadedDesign = loadGraphicTemplate(template);
 
-      
-      
+
+
       /* 
       const _scenes = template.scenes;
       let _scenes_new = template.scenes;
@@ -125,6 +129,9 @@ function App() {
       const updatedPreview = (await editor.renderer.render(updatedTemplate)) as string
       setCurrentPreview(updatedPreview)
       console.log("useEffect editor watcher");
+      
+      
+
     }
 
     if (editor) {
@@ -139,6 +146,29 @@ function App() {
 
 
   }, [editor]);
+
+  //点击画图的预览图
+  const changePage = React.useCallback(
+    async (page: any) => {
+      setCurrentPreview("")
+      if (editor) {
+        console.log("changePage");
+        const updatedTemplate = editor.scene.exportToJSON()
+        const updatedPreview = await editor.renderer.render(updatedTemplate)
+
+        const updatedPages = scenes.map((p) => {
+          if (p.id === updatedTemplate.id) {
+            return { ...updatedTemplate, preview: updatedPreview }
+          }
+          return p
+        }) as any[]
+        console.log('updatedPages',updatedPages);
+        setScenes(updatedPages)
+        setCurrentScene(page)
+      }
+    },
+    [editor, scenes, currentScene]
+  )
 
   //静态文字
   const addText = React.useCallback(() => {
@@ -400,7 +430,7 @@ function App() {
   }
 
   React.useEffect(() => {
-   // console.log("@");
+    // console.log("@");
     if (editor) {
       editor.objects.update({ opacity: opacity / 100 });
     }
@@ -565,14 +595,17 @@ function App() {
         {/*  <button onClick={addStaticPath}>ADD StaticPath</button> */}
         <button onClick={addElements}>ADD Elements</button>
       </div>
+      <div style={{ height: "102px", margin: "10px", display: "flex" }}>
+        {scenes.map((page, index) => (
+          <div key={index} onClick={changePage} style={page.id === currentScene.id ? { "width": "100px", "height": "100px", border: "solid 1px" } : { "width": "100px", "height": "100px" }}  >
+            <img style={{ "width": "100px", "height": "100px" }}  src={page.id === currentScene.id ? currentPreview : page.preview} />
+          </div>
+        ))}
+      </div>
       <div style={{ flex: 1, display: "flex" }}>
         <Canvas />
       </div>
-      <div style={{height:"100px", flex: 1, display: "flex" }}>
-      {scenes.map((page, index) => (
-              <img  style={{"width":"150px","height":"150px"}} key={index} src={page.id=== currentScene.id ? currentPreview : page.preview} />
-            ))}
-      </div>
+
     </div>
   );
 }
